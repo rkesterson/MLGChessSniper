ede9398f3fb38bdb935222f4f9e5405da22dd597
Ryan Kesterson <rkesterson1@atu.edu>
2017-03-30 20:34:27 -0500

    -Added many buttons, and 2d array

--- WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/MainForm.cs
@@ -15,6 +15,18 @@ namespace WindowsFormsApplication1
         public MainForm()
         {
             InitializeComponent();
+            positionArray[0, 0] = (int)peicePresidence.Pawn;
         }
+        //6 represents empty space
+        int[,] positionArray = new int[8, 8] { 
+            { 1, 2, 3, 4, 5, 3, 2, 1 },
+            { 0, 0, 0, 0, 0, 0, 0, 0 }, 
+            { 6, 6, 6, 6, 6, 6, 6, 6 },
+            { 6, 6, 6, 6, 6, 6, 6, 6 },
+            { 6, 6, 6, 6, 6, 6, 6, 6 },
+            { 6, 6, 6, 6, 6, 6, 6, 6 },
+            { 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 1, 2, 3, 5, 4, 3, 2, 1 } };
+        enum peicePresidence { Pawn, Castle, Knight, Bishop, Queen, King};
     }
 }
